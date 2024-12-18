250809c0547d531571317d1f05daf0bd48ce610c
Sampath Jaini <skjaini@gmail.com>
2024-12-18 15:44:32 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  // basePath: "/next_portfolio",
-  // assetPrefix: '/next_portfolio/',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  basePath: "/next_portfolio",
+  assetPrefix: '/next_portfolio/',
   images: {
     unoptimized: true,
   },
