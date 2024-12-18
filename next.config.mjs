9eb470cc94324dbe1c703d450c5ed6e9d293128a
Sampath Jaini <skjaini@gmail.com>
2024-12-18 15:06:41 -0800

    Update and rename next.config.ts to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
   // basePath: "/next_portfolio",
   // assetPrefix: '/next_portfolio/',
